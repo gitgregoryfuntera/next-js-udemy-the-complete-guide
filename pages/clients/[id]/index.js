90b0e59e2a8ed1ef686be78cfb20fed45ff6f5bf
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-04-21 19:40:49 +0800

    - added routing learnings

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
 const ClientProjectPage = () => {
-    return (
-        <div>ClientProjectPage works</div>
-    )
-}
+  const router = useRouter();;
+  const navigateTo = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "123" },
+    });
+  };
+  return (
+    <div>
+      ClientProjectPage works
+      <button onClick={navigateTo}>Click to navigate</button>
+    </div>
+  );
+};
 
 export default ClientProjectPage;
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 const ClientPage = () => {
-    return (
-        <div>ClientPage Works</div>
-    )
-}
+  const clients = [
+    { id: "max", name: "Max" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
+    <div>
+      ClientPage Works
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ClientPage;
