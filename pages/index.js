8eee2a04ff9a45378eecb332400c8f8336d15fae
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-05-04 18:53:17 +0800

    -added search event

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import EventList from "../components/events/event-list";
+import EventsSearch from "../components/events/events-search";
 import { getFeaturedEvents } from "../data/dummy-data";
 
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
   return (
     <div>
+      <EventsSearch />
       <EventList items={featuredEvents} />
     </div>
   );
