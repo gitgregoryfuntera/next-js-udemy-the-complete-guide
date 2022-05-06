f312f0b433647ed12a8ce4fac7259d3241cda6e5
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-05-07 06:44:41 +0800

    added submit event

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import EventList from "../components/events/event-list";
 import EventsSearch from "../components/events/events-search";
 import { getFeaturedEvents } from "../data/dummy-data";
-
+import { useRouter } from "next/router";
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
+  const router = useRouter();
+  const onSearch = (year, month) => {
+    const path = `/events/${year}/${month}`;
+    router.push(path);
+  };
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={onSearch} />
       <EventList items={featuredEvents} />
     </div>
   );
