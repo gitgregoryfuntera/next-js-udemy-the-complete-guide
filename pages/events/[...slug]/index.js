4bc3e2103d9add46bc378d6413ac63d837353a6f
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-05-08 08:10:28 +0800

    added event slug logic

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,6 +1,42 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../../data/dummy-data";
+import get from "lodash/get";
 
 const EventSlugPage = () => {
+  const router = useRouter();
+  const {
+    query: { slug },
+  } = router;
+
+  const yearValue = get(slug, "[0]");
+  const monthValue = get(slug, "[1]");
+
+  const numYear = +yearValue;
+  const numMonth = +monthValue;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents =
+    getFilteredEvents({
+      year: numYear,
+      month: numMonth,
+    }) || [];
+
+  const { length } = filteredEvents || [];
+
+  if (!length) {
+    return <p>No Events found for the choose filter</p>;
+  }
+
   return <div>EventSlugPage Works!</div>;
 };
 
