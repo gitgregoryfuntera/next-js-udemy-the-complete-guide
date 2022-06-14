0c42472babf94924e4c42722924716fd0f15ce68
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-06-14 18:06:27 +0800

    - added getstaticprops for events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import EventList from "../../components/events/event-list";
 import { getAllEvents } from "../../data/dummy-data";
+import { getEvents } from "../../utils/getEventsHelper";
 
-const EventsPage = () => {
-  const allEvents = getAllEvents();
+const EventsPage = (props) => {
+  const { allEvents } = props
   return <>
     <EventList items={allEvents}/>
   </>;
 };
 
 export default EventsPage;
+
+
+export const getStaticProps = async () => {
+  const events = await getEvents();
+  return {
+    props: {
+      allEvents: events
+    },
+    revalidate: 1800,
+  }
+}
