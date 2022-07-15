f483fa802da820acea2bd9f82d8ae7b7a5bd8736
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-07-15 18:54:55 +0800

    added mongo db connection

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,4 +1,5 @@
-const handle = (req, res) => {
+import { MongoClient } from "mongodb";
+const handle = async (req, res) => {
   const { method } = req;
   console.log("🚀 ~ file: index.js ~ line 6 ~ handle ~ method", method);
   if (method === "POST") {
@@ -6,6 +7,16 @@ const handle = (req, res) => {
       body: { email },
     } = req;
     console.log("🚀 ~ file: index.js ~ line 9 ~ handle ~ email", email);
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://dbUser:${"1234"}@cluster0.yqxng70.mongodb.net/newsletter?retryWrites=true&w=majority`
+    );
+
+    const db = await client.db();
+
+    await db.collection('emails').insertOne({ email });
+
+    client.close();
 
     res.json({
       email,
