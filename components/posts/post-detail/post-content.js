0d715945ac5b8a4fbea146999b6933ca2091882e
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-08-20 07:57:22 +0800

    added rendering of image using next js image tag

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,19 +1,35 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
-import ReactMarkdown from 'react-markdown';
-const DUMMY_POST = {
-  title: "Getting Started NextJS",
-  date: "2022-08-08",
-  image: "getting-started-nextjs.png",
-  slug: "getting-started-nextjs",
-  content: "# This is a first post",
-};
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customComponents = {
+    p: ({ node, children, ...props }) => {
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              height={200}
+              width={600}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,29 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = ({ post }) => {
+  return <PostContent post={post} />;
+};
+
+export const getStaticProps = (context) => {
+  const {
+    params: { slug },
+  } = context;
+  const post = getPostData(slug);
+  return {
+    props: {
+      post,
+    },
+    revalidate: 600,
+  };
+};
+export const getStaticPaths = () => {
+  const postFiles = getPostsFiles();
+  const slugs = postFiles.map((file) => file.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 };
 
 export default PostDetailPage;
