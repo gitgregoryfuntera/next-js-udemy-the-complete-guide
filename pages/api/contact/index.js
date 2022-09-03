8d06e32e9955e3cb87df7fda8f0fd01304f4d64e
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-09-04 06:44:07 +0800

    added DB connect to push messages

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,4 +1,5 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -18,15 +19,46 @@ const handler = (req, res) => {
     }
 
     const newMessage = {
-        email,
-        name,
-        message
+      email,
+      name,
+      message,
     };
 
-    console.log("🚀 ~ file: index.js ~ line 26 ~ handler ~ newMessage", newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://dbUser:${"rGlbGaMetb1RykIO"}@cluster0.yqxng70.mongodb.net/my-site?retryWrites=true&w=majority`
+      );
+
+      console.log(
+        "🚀 ~ file: index.js ~ line 26 ~ handler ~ newMessage",
+        newMessage
+      );
+    } catch (e) {
+      res.status(500).json({
+        message: "could not connect to DB",
+      });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (e) {
+      res.status(500).json({
+        message: "error inserting newMessage to DB",
+      });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({
-        message: 'success'
+      response: newMessage,
+      message: "success",
     });
   }
 };
