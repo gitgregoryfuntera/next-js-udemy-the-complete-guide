2de58c01537110a7bc97b93020400e41f9df5b84
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-11-19 07:09:47 +0800

    - added routing guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,9 +1,28 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    fetchSession();
+  }, []);
+
+  const fetchSession = async () => {
+    const session = await getSession();
+    if (!session) {
+      window.location.href = "/"
+    } else {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <>Loading...</>
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
