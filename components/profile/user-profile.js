6957fdd3466236759c730c5be783b71f11b56d15
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-11-20 08:23:42 +0800

    - fixed user props better routing

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,28 +1,28 @@
-import { getSession } from "next-auth/client";
-import { useEffect, useState } from "react";
+// import { getSession } from "next-auth/client";
+// import { useEffect, useState } from "react";
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    fetchSession();
-  }, []);
+  // useEffect(() => {
+  //   fetchSession();
+  // }, []);
 
-  const fetchSession = async () => {
-    const session = await getSession();
-    if (!session) {
-      window.location.href = "/"
-    } else {
-      setIsLoading(false);
-    }
-  };
+  // const fetchSession = async () => {
+  //   const session = await getSession();
+  //   if (!session) {
+  //     window.location.href = "/"
+  //   } else {
+  //     setIsLoading(false);
+  //   }
+  // };
 
-  if (isLoading) {
-    return <>Loading...</>
-  }
+  // if (isLoading) {
+  //   return <>Loading...</>
+  // }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
