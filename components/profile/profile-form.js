ac292bd16c6cf6cadc126973c9b583024c9ee4a2
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-12-10 06:31:02 +0800

    - added change password functionality

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,52 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
+import classes from "./profile-form.module.css";
 
 function ProfileForm() {
+  const newPasswordRef = useRef();
+  const oldPasswordRef = useRef();
+
+  const onHandleSubmit = async (e) => {
+    e.preventDefault();
+    const newPasswordVal = newPasswordRef?.current?.value;
+    const oldPasswordVal = oldPasswordRef?.current?.value;
+    const changePassArgs = {
+      oldPassword: oldPasswordVal,
+      newPassword: newPasswordVal,
+    };
+    console.log("🚀 ~ file: profile-form.js:16 ~ onHandleSubmit ~ changePassArgs", changePassArgs);
+    const result = await changePassword(changePassArgs);
+    console.log(
+      "🚀 ~ file: profile-form.js:17 ~ onHandleSubmit ~ result",
+      result
+    );
+  };
+
+  const changePassword = async (changePassArgs) => {
+    console.log(JSON.stringify(changePassArgs));
+    try {
+      const response = await fetch(`/api/user/change-password`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "PATCH",
+        body: JSON.stringify(changePassArgs),
+      });
+      const result = await response.json();
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onHandleSubmit}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,7 +1,12 @@
 import { getSession } from "next-auth/client";
+import { hashPassword, verifyPassword } from "../../../helper/auth";
+import { mongoDbClient } from "../../../helper/db";
 
 const handler = async (req, res) => {
-  const { method } = req;
+  const { method, body } = req;
+  console.log("🚀 ~ file: change-password.js:7 ~ handler ~ method", method);
+  console.log("🚀 ~ file: change-password.js:7 ~ handler ~ body", body);
+
   if (method !== "PATCH") {
     return;
   }
@@ -12,6 +17,45 @@ const handler = async (req, res) => {
     res.status(401).json({ message: "Not authenticated " });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = body.oldPassword;
+  const newPassword = body.newPassword;
+
+  const client = await mongoDbClient();
+  const usersCollection = client.db().collection("users");
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({
+      message: "User not found",
+    });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+  if (!passwordAreEqual) {
+    res.status(403).json({
+      message: "Password are not equal",
+    });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+  await usersCollection.updateOne(
+    {
+      email: userEmail,
+    },
+    { $set: { password: hashedPassword } }
+  );
+  client.close();
+
+  res.status(200).json({
+    message: "password updated!",
+  });
 };
 
 export default handler;
